7c991c922fcda1dce3f81b6d723a4eece9de331d
ashwabu <abdulazizredi90@gmail.com>
2023-07-12 22:01:47 +0300

    show video

--- script.py
+++ script.py
@@ -3,17 +3,25 @@ import cv2, time
 
 
 video = cv2.VideoCapture(0)
+while True:
+    check, frame = video.read()
 
-check, frame = video.read()
+    print(check)
+    print(frame)
 
-print(check)
-print(frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-time.sleep(3)
 
-cv2.imshow('frame',frame)
+    cv2.imshow('frame',gray)
 
-cv2.waitKey(0)
+    key=cv2.waitKey(1000)
+    
+    if key== ord('Q') or key== ord('q'):
+        break
+
+
+
+    
 
 video.release()
 
